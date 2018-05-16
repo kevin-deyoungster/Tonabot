dd54e8f41ee7941cb5a04f4fe64d56b2a1fb466f
kevin de Youngster <kevin.deyoungster@gmail.com>
2018-05-16 17:38:30 +0000

    Added confirm function to filter

--- tonabot.py
+++ tonabot.py
@@ -4,9 +4,6 @@ from contextlib import closing
 from bs4 import BeautifulSoup
 from urllib.parse import urlencode 
 
-
-
-
 def is_good_response(resp):
     content_type = resp.headers['Content-Type'].lower()
     return (resp.status_code == 200 
@@ -18,8 +15,8 @@ def log_error(e):
 tonaton = "https://tonaton.com"
 tonaton_url = "https://tonaton.com/en/ads/ghana/electronics?"
 
-def create_search_url(term):
-    params = {'query': term}
+def create_search_url(term,page_no):
+    params = {'query': term, 'page' : page_no}
     return tonaton_url + urlencode(params)
 
 def get_page(url):
@@ -55,23 +52,55 @@ def inspect_iphone(url):
             vendor = html.select('.poster a')[0].text
     return descr, condition, number, vendor
 
-def search(term):
-    search_url = create_search_url(term)
-    
+def confirm(text, rubric):
+    if rubric.lower() in text.lower():
+        return True
+    else:
+        return False
+
+def get_ad_details(content):
+    title = content.select('.item-title')[0].text
+    if confirm(title, ' 8 '):        
+        url = content.select('.item-title')[0]['href']
+        area = content.select('.item-area')[0].text
+        price = content.select('.item-info')[0].text
+        item = {'title' : title, 'location': area, 'price': price, 'url': url}
+        return item
+    else:
+        return None
+
+
+import pandas as pd
+
+def search_page(term, page_no):
+    search_url = create_search_url(term, page_no)    
     html = BeautifulSoup(get_page(search_url), 'html.parser')
     ads = html.select(".item-content")
     ads_dict = []
     for content in ads:
-        title = content.select('.item-title')[0].text
-        url = content.select('.item-title')[0]['href']
-        area = content.select('.item-area')[0].text
-        price = content.select('.item-info')[0].text
-        descr, condition, number, vendor = inspect_iphone(tonaton + url)
-        item = {'title' : title, 'location': area, 'price': price, 'url': url, 'description': descr, 'condition' : condition, 'number' : number, 'dealer' : vendor}
-        print(item['title'] + ' - ' + item['price'] + ' - ' + item['url'] + ' - ' + item['condition'] + ' - ' + str(item['dealer']) + ' - ' + item["number"])
-        ads_dict.append(item)
+        item = get_ad_details(content)
+        if(item):
+            descr, condition, number, vendor = inspect_iphone(tonaton + item['url'])
+            extras = {'description': descr, 'condition' : condition, 'number' : number, 'dealer' : vendor}
+            item = {**item, **extras}
+            del item['url']
+            del item['description']
+            # print(item['title'] + ' - ' + item['price'] + ' - ' + item['condition'] + ' - ' + str(item['dealer']) + ' - ' + item["number"])
+            ads_dict.append(item)
+    return ads_dict
 
 # print(inspect_iphone("https://tonaton.com/en/ad/apple-iphone-8-plus-256gb-new-for-sale-accra-25"))
-search("iphone 8+")
+def search(term, target_no):
+    result = []
+    for i in range(1, target_no):
+        # print("Page {}".format(i))
+        result = result + search_page(term, i)
+
+    stuff = pd.DataFrame(result)
+    print(stuff.to_string())
+    
+
+search("iphone 8 plus", 10)
+
 
 
