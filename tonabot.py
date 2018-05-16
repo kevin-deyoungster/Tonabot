71235db149d103e17bc7c65d1c541fc4de86eb7c
kevin de Youngster <kevin.deyoungster@gmail.com>
2018-05-16 15:46:29 +0000

    Added extraction for vendor name and numbers

--- tonabot.py
+++ tonabot.py
@@ -8,21 +8,11 @@ from urllib.parse import urlencode
 
 
 def is_good_response(resp):
-    """
-    Returns true if the response seems to be HTML, false otherwise
-    """
     content_type = resp.headers['Content-Type'].lower()
     return (resp.status_code == 200 
             and content_type is not None 
             and content_type.find('html') > -1)
-
-
 def log_error(e):
-    """
-    It is always a good idea to log errors. 
-    This function just prints them, but you can
-    make it do anything.
-    """
     print(e)
 
 tonaton = "https://tonaton.com"
@@ -33,17 +23,37 @@ def create_search_url(term):
     return tonaton_url + urlencode(params)
 
 def get_page(url):
-    with closing(get(url, stream=True)) as resp:
-        if is_good_response(resp):
-            return resp.content
-        else:
-            return None
+    try:
+        with closing(get(url, stream=True)) as resp:
+            if is_good_response(resp):
+                return resp.content
+            else:
+                return None
+    except RequestException as e:
+        log_error('Error during requests to {0} : {1}'.format(url, str(e)))
+        return None
 
 def inspect_iphone(url):
     html = BeautifulSoup(get_page(url), 'html.parser')
     descr = html.select('.item-description')[0].text
     condition = html.select('.item-properties dd')[0].text
-    return descr, condition
+    if html.select('.clearfix') is None:
+        number = 'None' 
+    else:
+        if len(html.select('.clearfix')) == 0:
+            number = 'None'
+        else:
+            number = html.select('.clearfix')[0].text
+
+    # print(html.select('.poster a'))
+    if html.select('.poster a') is None:
+        vendor =  html.select('.poster')[0].text.split("by")[1]
+    else:
+        if len(html.select('.poster a')) == 0:
+            vendor =  html.select('.poster')[0].text.split("by")[1]
+        else:
+            vendor = html.select('.poster a')[0].text
+    return descr, condition, number, vendor
 
 def search(term):
     search_url = create_search_url(term)
@@ -56,13 +66,12 @@ def search(term):
         url = content.select('.item-title')[0]['href']
         area = content.select('.item-area')[0].text
         price = content.select('.item-info')[0].text
-        descr, condition = inspect_iphone(tonaton + url)
-        item = {'title' : title, 'location': area, 'price': price, 'url': url, 'description': descr, 'condition' : condition}
+        descr, condition, number, vendor = inspect_iphone(tonaton + url)
+        item = {'title' : title, 'location': area, 'price': price, 'url': url, 'description': descr, 'condition' : condition, 'number' : number, 'dealer' : vendor}
+        print(item['title'] + ' - ' + item['price'] + ' - ' + item['url'] + ' - ' + item['condition'] + ' - ' + str(item['dealer']) + ' - ' + item["number"])
         ads_dict.append(item)
-    
-    for ad in ads_dict:
-        print(ad['title'] + ' - ' + ad['price'] + ' - ' + ad['url'] + ' - ' + ad['condition'])
 
+# print(inspect_iphone("https://tonaton.com/en/ad/apple-iphone-8-plus-256gb-new-for-sale-accra-25"))
 search("iphone 8+")
 
 
